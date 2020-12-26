01e0bb2693ed804132edab198c06accf59f44b34
rorro <rorro3025@outlook.com>
2020-12-26 12:18:53 -0600

    sing_in_finished

--- js/act_index_1.js
+++ js/act_index_1.js
@@ -21,22 +21,46 @@ const local_user = {
 const setUser = (user) => {
   if (user) {
     var user_active = auth.currentUser;
-    local_user.name = user_active.displayName;
+    //local_user.name = user_active.displayName;
     local_user.email = user_active.email;
     local_user.uid = user_active.uid;
     local_user.photoUrl = user_active.photoURL;
     local_user.emailVerified = user_active.emailVerified;
+    showUserByEmail(local_user.email.toLowerCase());
   } else {
     local_user.name = "Unknowed";
   }
 };
-// recopilar informacion 2 tomamos correo como conexion entre BD_firestrore y BD_ususarios 
-const setUserFromFiresore = async (email) => {
-  var doc = await getStudent(email);
-  var student = doc.data();
-  local_user.name = student.Nombre;
-  local_user.career = student.Carrera;
-  local_user.account_numer = student.No_cuenta;
+// recopilar informacion 2, tomamos correo como conexion entre BD_firestrore y BD_ususarios 
+// consulta por email 
+const showUserByEmail = async (email) => {
+  fs.collection("students").where("Email", "==", email)
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        //console.log(doc.id, " => ", doc.data());
+        var student = doc.data();
+        local_user.name = student.Nombre;
+        local_user.career = student.Carrera;
+        local_user.account_numer = student.NumCuenta;
+        console.log(local_user.name,local_user.career,local_user.account_numer);
+        const say_N = document.querySelector('#title_admin');
+        const user_info = document.querySelector('#info_user');
+        say_N.innerHTML = `Hola usuario`;
+        let html_value = `
+                        <li> Numero de cuenta ${local_user.account_numer} </li>
+                        <li> Nombre completo ${local_user.name} </li>
+                        <li> Correo electronico ${local_user.email} </li> 
+                        <li> Carrera: ${local_user.career} </li>
+                        <li> Verifico email: ${local_user.emailVerified} </li>
+                        `;
+        user_info.innerHTML = html_value;
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents xD: ", error);
+    });
 }
 
 // Cerrar sesion 
@@ -54,23 +78,11 @@ logout.addEventListener('click', e => {
 
 // informacion del usuario
 //Estado de usuario
-auth.onAuthStateChanged (async (user) => {
+auth.onAuthStateChanged((user) => {
   if (user) {
     setUser(user);
-    await setUserFromFiresore("hL4UJEsIITomzuuykJva");
     console.log("Existe usuario activo name: " + local_user.name, "Email: " + local_user.email);
     //ID Sandra("BFhfhzXQvsHLQj6t1OQO") ID Rodrigo("hL4UJEsIITomzuuykJva");
-    const say_N = document.querySelector('#title_admin');
-    const user_info = document.querySelector('#info_user');
-    say_N.innerHTML = `Hola usuario`;
-    let html_value = `
-                        <li> Numero de cuenta ${local_user.account_numer} </li>
-                        <li> Nombre completo ${local_user.name} </li>
-                        <li> Correo electronico ${local_user.email} </li> 
-                        <li> Carrera: ${local_user.career} </li>
-                        <li> Verifico email: ${local_user.emailVerified} </li>
-                        `;
-    user_info.innerHTML = html_value;
   } else {
     console.log("no hay usuario activo");
   }
