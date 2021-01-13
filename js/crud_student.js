c433095569d50c2bec8c0ee5d7f58b36690133a3
rorro <rorro3025@outlook.com>
2021-01-13 14:44:54 -0600

    CRUD_1_V_0.1

--- js/crud_student.js
+++ js/crud_student.js
@@ -56,9 +56,9 @@ const showUserByEmail = async (email) => {
 /*Alta de un estudiante primero intentando el registro de 
 un usuario y luego manando la informacion del formulario a la DB de firestore*/
 
-const form_student = document.querySelector("#form_student");
+const form_student_create = document.querySelector("#form_student_create");
 
-form_student.addEventListener('submit', (e) => {
+form_student_create.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.querySelector("#email_form_student").value;
     const name = document.querySelector("#name_form_student").value;
@@ -66,11 +66,11 @@ form_student.addEventListener('submit', (e) => {
     const career = document.querySelector("#career_form_student").value;
     const password = document.querySelector("#password_form_student").value;
     const password_c = document.querySelector("#password_2_form_student").value;
-if(password == password_c){
-    auth
-        .createUserWithEmailAndPassword(email, password)
-        .then(userCredencial => {
-            
+    if (password == password_c) {
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then(userCredencial => {
+
                 fs.collection("users").add({
                     Nombre: name,
                     Correo: email,
@@ -81,51 +81,98 @@ if(password == password_c){
                     .then(function (docRef) {
                         console.log("Document written with ID: ", docRef.id);
                         console.log("registrado");
-                        form_student.reset();
+                        form_student_create.reset();
                     })
                     .catch(function (error) {
                         console.error("Error adding document: ", error);
                     });
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode == 'auth/email-already-in-use') {
+                    alert('El correo ya a sido utilizado');
+                    $('#singup-window').modal('hide');
+                } else {
+                    alert(errorMessage);
+                    $('#singup-window').modal('hide');
+                }
+                console.log(error);
+            });
+    } else {
+        alert("las contraseñas no coinciden");
+    }
+});
+// busqueda de usuario (alumnos) con numero de cuenta
+const form_student_search = document.querySelector("#form_student_search");
+var container = document.querySelector("#result_student");
+var html_value = "undefineed DX";
+form_student_search.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const no_account = document.querySelector("#no_account_txtbox_student").value;
+    console.log(no_account);
+    container.innerHTML = no_account;
+    fs.collection("users").where("NumCuenta", "==", 418070695)
+        .get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                //console.log(doc.id, " => ", doc.data());
+                var student = doc.data();
+                var id_bot = student.id;
+                html_value = `
+                        <form class="row g-3" id="form_student_update">
+                        <div class="col-md-6"> 
+                            <label for="name_form_student_update" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" id="name_form_student_update" required>
+                            <label for="no_cuenta_form_student_update" class="form-label">Numero de cuenta</label>
+                            <input type="number" class="form-control" id="no_cuenta_form_student_update" required>
+                            <label for="carrer_form_student_update" class="form-label">Carrera</label>
+                            <input type="text" class="form-control" id="career_form_student_update" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="email_form_student_update" class="form-label">Correo electronico</label>
+                            <input type="email" class="form-control" id="email_form_student_update" required>
+                        </div>
+                        <br>
+                        <div class="col-12">
+                        <button class="btn btn-primary borrar_bot" data-id="${id_bot}">Eliminar</button>
+                        <button class="btn btn-secondary update_bot" data-id="${id_bot}">Editar</button>
+                        </div>
+                    </form>
+                        `;
+            });
+            container.innerHTML = html_value;
         })
         .catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode == 'auth/email-already-in-use') {
-                alert('El correo ya a sido utilizado');
-                $('#singup-window').modal('hide');
-            } else {
-                alert(errorMessage);
-                $('#singup-window').modal('hide');
-            }
-            console.log(error);
+            container.innerHTML = "NO se encontraron conincidencias";
+            console.log("Error getting documents xD: ", error);
         });
-}else{
-alert("las contraseñas no coinciden");
-}
+
 });
 
 // Cerrar sesion 
 const logout = document.querySelector('#logout-btn');
 logout.addEventListener('click', e => {
-  e.preventDefault();
-  auth
-    .signOut()
-    .then(() => {
-      console.log("Cerraste sesion");
-      $('#close-question').modal('hide');
-      window.location = "index.html";
-    });
+    e.preventDefault();
+    auth
+        .signOut()
+        .then(() => {
+            console.log("Cerraste sesion");
+            $('#close-question').modal('hide');
+            window.location = "index.html";
+        });
 });
 
 // informacion del usuario
 //Estado de usuario
 auth.onAuthStateChanged((user) => {
-  if (user) {
-    setUser(user);
-    console.log("Existe usuario activo name: " + local_user.name, "Email: " + local_user.email);
-    //ID Sandra("BFhfhzXQvsHLQj6t1OQO") ID Rodrigo("hL4UJEsIITomzuuykJva");
-  } else {
-    console.log("no hay usuario activo");
-  }
+    if (user) {
+        setUser(user);
+        console.log("Existe usuario activo name: " + local_user.name, "Email: " + local_user.email);
+        //ID Sandra("BFhfhzXQvsHLQj6t1OQO") ID Rodrigo("hL4UJEsIITomzuuykJva");
+    } else {
+        console.log("no hay usuario activo");
+    }
 });
